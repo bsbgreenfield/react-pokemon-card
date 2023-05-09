b122ed122a3f599a24e88be921155ea786c3f03c
Benjamin Greenfield <benjamingreenfield@Benjamins-MacBook-Pro.local>
2023-05-09 10:35:07 -0700

    init

---
+++ src/hooks.js
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+
+const useFlip = () => {
+    const [isFlipped, setIsFlipped] = useState(true);
+    const toggleFlip = () => {
+        setIsFlipped(isFlipped => !isFlipped)
+    }
+    return [isFlipped, toggleFlip]
+}
+
+export default useFlip
